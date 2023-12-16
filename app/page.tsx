122987ad4b9cb7dfa3a6f1a6c058d458e5055815
Angelo Ilardi <ange448@gmail.com>
2023-12-16 19:43:20 +0000

    added dashboard for desktop

--- app/page.tsx
+++ app/page.tsx
@@ -64,13 +64,14 @@ export default function Home() {
     <div className="flex flex-col max-h-full">
       <div className="h-[300px] bg-[url(/images/pattern-bg-mobile.png)] bg-no-repeat bg-cover overflow-visible">
         <h1 className="text-center text-3xl py-8">IP Address Tracker</h1>
-        <div className="flex rounded-md w-full h-[60px] px-6">
+        <div className="flex rounded-md max-w-[556px] h-[60px] px-6 justify-center mx-auto">
           <input
             type="text"
             name="ip-address"
             id="ip-address"
             onChange={(e) => setIpAddress(e.target.value)}
             className="rounded-s-lg w-full text-very-dark-gray"
+            placeholder="Search for an IP address or domain"
             // pattern="(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)_*(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)_*){3}"
           />
           <button
@@ -81,26 +82,28 @@ export default function Home() {
             <Image src="/images/icon-arrow.svg" width={15} height={15} alt="" />
           </button>
         </div>
-        <div className="bg-white text-black mx-6 mt-6 rounded-lg text-center relative top-0 bottom-0 z-50 flex flex-col gap-5 py-6">
-          <div>
+        <div className="bg-white text-black mx-6 mt-6 rounded-lg text-center relative top-0 bottom-0 z-50 flex flex-col gap-5 py-6 md:flex-row md:text-left md:justify-evenly md:items-start">
+          <div className="md:px-[30px] md:py-10 flex-1">
             <p className="text-dark-gray text-xs font-semibold">IP ADDRESS</p>
-            <p className="text-very-dark-gray font-medium text-lg">{results.ipAddress}</p>
+            <p className="text-very-dark-gray font-medium text-lg md:text-2xl">
+              {results.ipAddress}
+            </p>
           </div>
-          <div>
+          <div className="md:px-[30px] md:py-10 md:border-l-2 flex-1">
             <p className="text-dark-gray text-xs font-semibold">LOCATION</p>
-            <p className="text-very-dark-gray font-medium text-lg">
+            <p className="text-very-dark-gray font-medium text-lg md:text-2xl">
               {results.location}, {results.region}, {results.country}
             </p>
           </div>
-          <div>
+          <div className="md:px-[30px] md:py-10 md:border-l-2 flex-1">
             <p className="text-dark-gray text-xs font-semibold">TIMEZONE</p>
-            <p className="text-very-dark-gray font-medium text-lg">
+            <p className="text-very-dark-gray font-medium text-lg md:text-2xl">
               UTC {results.timezone}
             </p>
           </div>
-          <div>
+          <div className="md:px-[30px] md:py-10 md:border-l-2 flex-1">
             <p className="text-dark-gray text-xs font-semibold">ISP</p>
-            <p className="text-very-dark-gray font-medium text-lg">
+            <p className="text-very-dark-gray font-medium text-lg md:text-2xl">
               {results.isp}
             </p>
           </div>
